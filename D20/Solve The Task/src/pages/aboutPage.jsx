b20bc22aa9a7068d57fee366ee26e1a398a46eb3
salahsaed5 <salahsaed599@gmail.com>
2023-09-09 02:06:48 +0300

    update color

--- D20/Solve The Task/src/pages/aboutPage.jsx
+++ D20/Solve The Task/src/pages/aboutPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AboutPage = () => {
     return (
         <div className='App-header'>
-            <h1 style={{color:"white",fontFamily:"revert-layer" ,fontSize:55 ,textShadow: '5px 2px 3px red',width:1000}}><center>
+            <h1 style={{color:"white",fontFamily:"revert-layer" ,fontSize:55 ,textShadow: '5px 2px 20px brown',width:1090}}><center>
             موقع نيتفليكس يتيح لك مشاهدة و تحميل الافلام والمسلسلات مجانا حيث يوجد به افلام ومسلسلات جديدة و قديمة ونادرة. و الموقع يعمل على جميع الاجهزة ولا يحتاج ل انترنت سريع            </center>
                 
               </h1>
